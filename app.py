15569e1d0333ad74c19b57a4c0187701a575f609
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2023-04-02 15:35:04 +0200

    sad

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,request, jsonify
 import numpy as np
 from scipy.fft import fft, rfft
 from scipy.fft import fftfreq, rfftfreq
+from scipy.signal import find_peaks
 
 app = Flask(__name__)
 
@@ -12,32 +13,28 @@ def Sampling_Studio():
 @app.route('/calculate-fft-max', methods=['POST'])
 def calculate_fft_max():
     array = request.get_json()
-    # fft = np.fft.fft(array)
-# Get the magnitudes of the FFT coefficients
-    # fft_magnitudes = np.abs(fft)
-
-    # # Get the frequencies corresponding to the FFT coefficients
-    # sampling_rate = 1000  # This is the number of samples per second
-    # frequencies = np.fft.fftfreq(len(fft_magnitudes), 1 / sampling_rate)
-
-    # # Get the index of the maximum magnitude of the FFT coefficients
-    # max_magnitude_index = np.argmax(fft_magnitudes)
-
-    # # Get the frequency corresponding to the maximum magnitude
-    # max_frequency = frequencies[max_magnitude_index]
-    fourier = fft(array)
-    N = len(array)
-    normalize = N/2
-    sampling_rate = 1000.0 # It's used as a sample spacing
-    frequency_axis = fftfreq(N, d=1.0/sampling_rate)
-    norm_amplitude = np.abs(fourier)/normalize
     
-    indices = np.where(norm_amplitude >= 1.0)
-    max_freq_index = indices[0][np.argmax(norm_amplitude[indices])]
+    # fourier = fft(array)
+    # N = len(array)
+    # normalize = N/2
+    # sampling_rate = 1000.0 # It's used as a sample spacing
+    # frequency_axis = fftfreq(N, d=1.0/sampling_rate)
+    # norm_amplitude = np.abs(fourier)/normalize
+    
+    # indices = np.where(norm_amplitude >= 1.0)
+    # max_freq_index = indices[0][np.argmax(norm_amplitude[indices])]
 
-    max_frequency = frequency_axis[max_freq_index]
-    print(max_frequency)
-
+    # max_frequency = frequency_axis[max_freq_index]
+    # print(max_frequency)
+    fft_signal = fft(array) # Perform the FFT on the signal
+    freqs = np.fft.fftfreq(len(array),d=1/1000) # Get the corresponding frequency values
+    abs_fft_signal = np.abs(freqs) # Get the absolute value of the FFT signal
+    peaks, _ = find_peaks(abs_fft_signal) # Find the indices of the peaks
+    print(freqs[peaks[len(peaks)-1]])
+    max_frequency=1
+    # max_freq_index = np.argmax(np.abs(fft_signal)) # Find the index of the highest amplitude component
+    # # max_frequency = freqs[max_freq_index] # Get the frequency value of the highest amplitude component
+    # max_frequency = np.max(freqs) # Get the frequency value of the highest amplitude component
 
     
     
